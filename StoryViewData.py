fee6aed0759b189a4c81ec362ac6e23e88ff7729
ghh tiran <ghhtiran@gmail.com>
2024-10-14 05:46:21 +0330

    5:54 7/23

--- StoryViewData.py
+++ StoryViewData.py
@@ -2,17 +2,18 @@ import pyautogui as pyag
 from pyperclip import paste
 # from time import sleep
 
-pyag.PAUSE = 0.09
+pyag.PAUSE = 0.07
 def dataCapturer(storyNumber,NumberOfTurns):
     dataFile = open('firstPhase/'+ 'story' + storyNumber+'data.txt','a+',encoding='utf-8')
     for i in range(NumberOfTurns):
-        pyag.click(x=410, y=250)
-        pyag.click(x=410, y=250)
-        pyag.moveTo(410,400)
+        # ______laptop shercat
+        # pyag.click(x=410, y=250)
+        # pyag.click(x=410, y=250)
+        # pyag.moveTo(410,400)
         # ______system ghaii dada
-        # pyag.click(x=2500, y=600) 
-        # pyag.click(x=2500, y=600)
-        # pyag.moveTo(2600,800)
+        pyag.click(x=2500, y=600) 
+        pyag.click(x=2500, y=600)
+        pyag.moveTo(2600,800)
         # ______system shercat
         # pyag.click(x=-900, y=414)
         # pyag.click(x=-900, y=414)
@@ -22,8 +23,8 @@ def dataCapturer(storyNumber,NumberOfTurns):
         dataFile.write(paste())
         # print(paste())
         # _______scroll windows 
-        # pyag.scroll(-1000)
+        pyag.scroll(-1000)
         # _______Scroll linux 
-        pyag.scroll(-7)
+        # pyag.scroll(-7)
 
     dataFile.close()
