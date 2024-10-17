cf2ac361acc00ef2a1cd8b8e9b59ed576f8af02b
ghh Tiran <ghhTiran@gmail.com>
2024-10-17 13:22:27 +0330

    13:22 7/27

--- StoryViewData.py
+++ StoryViewData.py
@@ -7,13 +7,13 @@ def dataCapturer(storyNumber,NumberOfTurns):
     dataFile = open('firstPhase/'+ 'story' + storyNumber+'data.txt','a+',encoding='utf-8')
     for i in range(NumberOfTurns):
         # ______laptop shercat
-        # pyag.click(x=410, y=250)
-        # pyag.click(x=410, y=250)
-        # pyag.moveTo(410,400)
+        pyag.click(x=410, y=250)
+        pyag.click(x=410, y=250)
+        pyag.moveTo(410,400)
         # ______system ghaii dada
-        pyag.click(x=2500, y=600) 
-        pyag.click(x=2500, y=600)
-        pyag.moveTo(2600,800)
+        # pyag.click(x=2500, y=600) 
+        # pyag.click(x=2500, y=600)
+        # pyag.moveTo(2600,800)
         # ______system shercat
         # pyag.click(x=-900, y=414)
         # pyag.click(x=-900, y=414)
@@ -23,8 +23,8 @@ def dataCapturer(storyNumber,NumberOfTurns):
         dataFile.write(paste())
         # print(paste())
         # _______scroll windows 
-        pyag.scroll(-1000)
+        # pyag.scroll(-1000)
         # _______Scroll linux 
-        # pyag.scroll(-5)
+        pyag.scroll(-5)
 
     dataFile.close()
